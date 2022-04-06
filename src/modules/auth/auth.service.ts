94691e9f099c612a41e11dce45fc7b93bda3272b
mauricioaznar <mauricioaznar94@gmail.com>
2022-04-06 13:42:22 -0500

    auth

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -24,7 +24,11 @@ export class AuthService {
     if (!user) {
       return null;
     }
-    const isMatch = await bcrypt.compare(pass, user.password);
+    let storedPassword = user.password;
+    if (user.password.match(/^\$2y(.+)$/i)) {
+      storedPassword = user.password.replace(/^\$2y(.+)$/i, '$2a$1');
+    }
+    const isMatch = await bcrypt.compare(pass, storedPassword);
     if (!isMatch) {
       return null;
     }
