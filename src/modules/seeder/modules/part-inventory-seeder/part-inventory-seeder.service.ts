ee55f176de16b5ec45f3cac4cd3496d75d1fa0d3
mauricioaznar <mauricioaznar94@gmail.com>
2022-04-10 15:00:02 -0500

    more seeds

--- src/modules/seeder/modules/part-inventory-seeder/part-inventory-seeder.service.ts
+++ src/modules/seeder/modules/part-inventory-seeder/part-inventory-seeder.service.ts
@@ -16,5 +16,45 @@ export class PartInventorySeederService {
       part_id: partsSeed.materials.banda700.id,
       quantity: 4,
     });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.banda600.id,
+      quantity: 2,
+    });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.banda800.id,
+      quantity: 10,
+    });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.contactor500.id,
+      quantity: 2,
+    });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.contactor400.id,
+      quantity: 1,
+    });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.tornillo3.id,
+      quantity: 30,
+    });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.tornillo1.id,
+      quantity: 10,
+    });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.resistencia20.id,
+      quantity: 2,
+    });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.resistencia30.id,
+      quantity: 1,
+    });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.gomas1.id,
+      quantity: 50,
+    });
+    await this.partInventoryService.add({
+      part_id: partsSeed.materials.gomas2.id,
+      quantity: 80,
+    });
   }
 }
