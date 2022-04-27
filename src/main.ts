fadda7bc1d177fc05951fff4493f2e03da9c5da3
mauricioaznar <mauricioaznar94@gmail.com>
2022-04-27 16:06:42 -0500

    more

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import dayjs from 'dayjs';
 import utcPlugin from 'dayjs/plugin/utc';
 
@@ -9,12 +8,6 @@ dayjs.extend(utcPlugin);
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: false, // white list should be false otherwise fields that are not specified in 'class validator' are going to get filtered
-            transform: true,
-        }),
-    );
     await app.listen(3008);
 }
 
