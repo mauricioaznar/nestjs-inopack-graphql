6c233c882c04d2e384e5cf7ac47d9020ee614f6c
mauricioaznar <mauricioaznar94@gmail.com>
2022-04-28 17:01:06 -0500

    remove moment

--- src/modules/entities/production/products/products.service.ts
+++ src/modules/entities/production/products/products.service.ts
@@ -1,7 +1,8 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../../common/services/prisma/prisma.service';
 import { Product, ProductUpsertInput } from '../../../../common/dto/entities';
-import { minLength } from 'class-validator';
+import { isEmpty, isNotEmpty, minLength } from 'class-validator';
+import { isRequiredArgument } from 'graphql';
 
 @Injectable()
 export class ProductsService {
@@ -26,9 +27,10 @@ export class ProductsService {
     }
 
     async upsertInput(input: ProductUpsertInput): Promise<Product> {
-        const codeMinLength = minLength(input.code, 10);
-        if (!codeMinLength) {
-            throw new BadRequestException(['Min length is not enough']);
+        const errors = await this.validateUpsertInput(input);
+
+        if (errors.length > 0) {
+            throw new BadRequestException(errors);
         }
 
         return this.prisma.products.upsert({
@@ -60,5 +62,89 @@ export class ProductsService {
                 id: input.id || 0,
             },
         });
+    }
+
+    async validateUpsertInput(input: ProductUpsertInput): Promise<string[]> {
+        const errors: string[] = [];
+
+        // width
+        if (ProductsService.isWidthRequired(input) && isEmpty(input.width)) {
+            errors.push('Width is required');
+        }
+
+        // length
+        if (ProductsService.isLengthRequired(input) && isEmpty(input.length)) {
+            errors.push('Length is required');
+        }
+
+        // current group weight
+        if (
+            ProductsService.isCurrentGroupWeightRequired(input) &&
+            isEmpty(input.current_group_weight)
+        ) {
+            errors.push('Current group weight is required');
+        }
+
+        // calibre
+        if (
+            ProductsService.isCalibreRequired(input) &&
+            isEmpty(input.calibre)
+        ) {
+            errors.push('Calibre is required');
+        }
+
+        // packing
+        if (
+            ProductsService.isPackingIdRequired(input) &&
+            isEmpty(input.packing_id)
+        ) {
+            errors.push('Packing is required');
+        }
+
+        return errors;
+    }
+
+    static isBag(input: ProductUpsertInput) {
+        return input.order_production_type_id === 1;
+    }
+
+    static isRoll(input: ProductUpsertInput) {
+        return input.order_production_type_id === 2;
+    }
+
+    static isPellet(input: ProductUpsertInput) {
+        return input.order_production_type_id === 3;
+    }
+
+    static isOthers(input: ProductUpsertInput) {
+        return input.order_production_type_id === null;
+    }
+
+    static isWidthRequired(input: ProductUpsertInput) {
+        return (
+            ProductsService.isBag(input) ||
+            ProductsService.isRoll(input) ||
+            ProductsService.isOthers(input)
+        );
+    }
+
+    static isLengthRequired(input: ProductUpsertInput) {
+        return ProductsService.isBag(input) || ProductsService.isOthers(input);
+    }
+
+    static isCurrentGroupWeightRequired(input: ProductUpsertInput) {
+        return ProductsService.isBag(input) || ProductsService.isOthers(input);
+    }
+
+    static isCalibreRequired(input: ProductUpsertInput) {
+        return (
+            ProductsService.isBag(input) ||
+            ProductsService.isRoll(input) ||
+            ProductsService.isOthers(input)
+        );
+    }
+
+    static isPackingIdRequired(input: ProductUpsertInput) {
+        return ProductsService.isBag(input) || ProductsService.isRoll(input);
     }
 }
