266791fc2fb53aceb524b073a36882964438c222
mauricioaznar <mauricioaznar94@gmail.com>
2022-06-28 15:15:12 -0500

    product inventory

--- src/common/dto/entities/summaries/production-summary.dto.ts
+++ src/common/dto/entities/summaries/production-summary.dto.ts
@@ -1,8 +1,20 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, registerEnumType } from '@nestjs/graphql';
 import { DateGroup } from '../../dates/dates';
 
 @InputType('ProductionSummaryArgs')
 export class ProductionSummaryArgs {
     @Field(() => DateGroup, { nullable: false })
     date_group: DateGroup;
+
+    @Field(() => ProductionSummaryEntitiesGroup, { nullable: false })
+    entity_group: ProductionSummaryEntitiesGroup;
 }
+
+export enum ProductionSummaryEntitiesGroup {
+    product = 'product',
+    machine = 'machine',
+}
+
+registerEnumType(ProductionSummaryEntitiesGroup, {
+    name: 'ProductionSummaryEntitiesGroup',
+});
