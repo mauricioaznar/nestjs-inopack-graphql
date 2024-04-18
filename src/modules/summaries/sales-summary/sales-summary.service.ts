128f92043695433ef7bc5b5b6f5817acce4916d7
mauricioaznar <mauricioaznar94@gmail.com>
2024-04-17 18:02:13 -0600

    patch for paginated order sales.

--- src/modules/summaries/sales-summary/sales-summary.service.ts
+++ src/modules/summaries/sales-summary/sales-summary.service.ts
@@ -102,10 +102,10 @@ export class SalesSummaryService {
                  receipt_types.name receipt_type_name,
                  order_sale_statuses.id status_id,
                  order_sale_statuses.name status_name,
-                 osp.kilos kilos_sold,
-                 ((osp.kilos * osp.kilo_price) - (osp.kilos * osp.kilo_price * osp.discount / 100) + (osp.groups * osp.group_price) - (osp.groups * osp.group_price * osp.discount / 100)) total,
-                 ((osp.kilos * osp.kilo_price) - (osp.kilos * osp.kilo_price * osp.discount / 100) + (osp.groups * osp.group_price) - (osp.groups * osp.group_price * osp.discount / 100)) * IF(order_sales.receipt_type_id = 2, 0.16, 0) tax,
-                 ((osp.kilos * osp.kilo_price) - (osp.kilos * osp.kilo_price * osp.discount / 100) + (osp.groups * osp.group_price) - (osp.groups * osp.group_price * osp.discount / 100)) * IF(order_sales.receipt_type_id = 2, 1.16, 1) total_with_tax
+                 (osp.kilos - ifnull(asp.kilos, 0)) kilos_sold,
+                 (((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price) - ((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price * osp.discount / 100) + ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price) - ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price * osp.discount / 100)) total,
+                 (((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price) - ((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price * osp.discount / 100) + ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price) - ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price * osp.discount / 100)) * IF(order_sales.receipt_type_id = 2, 0.16, 0) tax,
+                 (((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price) - ((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price * osp.discount / 100) + ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price) - ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price * osp.discount / 100)) * IF(order_sales.receipt_type_id = 2, 1.16, 1) total_with_tax
             from order_sales
                 join order_sale_products as osp
                 on osp.order_sale_id = order_sales.id
@@ -123,6 +123,24 @@ export class SalesSummaryService {
                 on order_sale_statuses.id = order_sales.order_sale_status_id
                 left join receipt_types
                 on receipt_types.id = order_sales.receipt_type_id
+                left join (
+                    select 
+                        order_adjustment_products.product_id as product_id,
+                        order_adjustments.order_sale_id as order_sale_id,
+                        sum(order_adjustment_products.kilos) as kilos,
+                        sum(order_adjustment_products.groups) as groups
+                    from order_adjustments
+                        join order_adjustment_products
+                        on order_adjustment_products.order_adjustment_id = order_adjustments.id
+                        and order_adjustment_products.active = 1
+                     group by order_adjustments.order_sale_id, order_adjustment_products.product_id
+                     where order_adjustments.active = 1 
+      
+                
+                ) as asp 
+                    on asp.order_sale_id = order_sales.id
+                    and asp.product_id = osp.product_id
+                
             where osp.active = 1
               and order_sales.active = 1
                 ) as ctc
@@ -132,6 +150,69 @@ export class SalesSummaryService {
             group by ${groupByEntityGroup} ${groupByDateGroup}
             order by ${orderByDateGroup}
         `);
+
+        /*
+              const sales = await this.prisma.$queryRawUnsafe<SalesSummary['sales']>(`
+            select sum(ctc.kilos_sold)                  as               kilos_sold,
+                   sum(ctc.total)                       as               total,
+                   sum(ctc.tax)                         as               tax,
+                   sum(ctc.total_with_tax)              as               total_with_tax,
+                   ${selectEntityGroup}
+                   ${selectDateGroup}
+            from (
+             select
+                 date (date_add(order_sales.date, interval -WEEKDAY(order_sales.date) - 1 day)) first_day_of_the_week,
+                 date(date_add(date_add(order_sales.date, interval -WEEKDAY(order_sales.date) - 1 day), interval 6 day)) last_day_of_the_week,
+                 order_sales.date start_date,
+                 products.id product_id,
+                 products.description product_name,
+                 products.order_production_type_id order_production_type_id,
+                 order_production_type.name order_production_type_name,
+                 product_categories.id product_category_id,
+                 product_categories.name product_category_name,
+                 accounts.id account_id,
+                 accounts.name account_name,
+                 accounts.abbreviation account_abbreviation,
+                 receipt_types.id receipt_type_id,
+                 receipt_types.name receipt_type_name,
+                 order_sale_statuses.id status_id,
+                 order_sale_statuses.name status_name,
+                 (osp.kilos) kilos_sold,
+                 (((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price) - ((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price * osp.discount / 100) + ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price) - ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price * osp.discount / 100)) total,
+                 (((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price) - ((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price * osp.discount / 100) + ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price) - ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price * osp.discount / 100)) * IF(order_sales.receipt_type_id = 2, 0.16, 0) tax,
+                 (((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price) - ((osp.kilos - ifnull(asp.kilos, 0)) * osp.kilo_price * osp.discount / 100) + ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price) - ((osp.groups - ifnull(asp.groups, 0)) * osp.group_price * osp.discount / 100)) * IF(order_sales.receipt_type_id = 2, 1.16, 1) total_with_tax
+            from order_sales
+                join order_sale_products as osp
+                on osp.order_sale_id = order_sales.id
+                left join order_requests
+                on order_requests.id = order_sales.order_request_id
+                left join products
+                on osp.product_id = products.id
+                left join order_production_type
+                on order_production_type.id = products.order_production_type_id
+                left join product_categories
+                on products.product_category_id = product_categories.id
+                left join accounts
+                on accounts.id = order_sales.account_id
+                left join order_sale_statuses
+                on order_sale_statuses.id = order_sales.order_sale_status_id
+                left join receipt_types
+                on receipt_types.id = order_sales.receipt_type_id
+                left join order_adjustments
+                on order_adjustments.order_sale_id = order_sales.id
+                left join order_adjustment_products as asp
+                on asp.product_id = osp.product_id and osp.order_sale_id = order_adjustments.order_sale_id
+            where osp.active = 1
+              and order_sales.active = 1
+                ) as ctc
+            where ctc.start_date >= '${startDate}'
+              and ctc.start_date < '${endDate}'
+              ${ownProductWhere}
+            group by ${groupByEntityGroup} ${groupByDateGroup}
+            order by ${orderByDateGroup}
+        `);
+
+         */
 
         return {
             sales: sales,
