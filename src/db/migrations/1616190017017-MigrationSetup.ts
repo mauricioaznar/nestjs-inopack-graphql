60a4b4c256a9a181ff8bc39dd102d902f22d30c2
mau <mauricioaznar94@gmail.com>
2026-06-06 23:42:22 -0600

    Revert "fix: make MigrationSetup idempotent for fresh databases"
    This reverts commit 627c62a3f4bfdad095a0b59cb2420ec41149cd25.

--- src/db/migrations/1616190017017-MigrationSetup.ts
+++ src/db/migrations/1616190017017-MigrationSetup.ts
@@ -3,17 +3,15 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class MigrationSetup1616190017017 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'ALTER TABLE `migrations` ADD COLUMN IF NOT EXISTS `timestamp` bigint NOT NULL;',
+      'ALTER TABLE `migrations` ADD COLUMN `timestamp` bigint NOT NULL;',
     );
     await queryRunner.query(
-      'ALTER TABLE `migrations` ADD COLUMN IF NOT EXISTS `name` VARCHAR(255) NOT NULL;',
+      'ALTER TABLE `migrations` ADD COLUMN `name` VARCHAR(255) NOT NULL;',
     );
     await queryRunner.query(
-      'ALTER TABLE `migrations` DROP COLUMN IF EXISTS `migration`;',
+      'ALTER TABLE `migrations` DROP COLUMN `migration`;',
     );
-    await queryRunner.query(
-      'ALTER TABLE `migrations` DROP COLUMN IF EXISTS `batch`;',
-    );
+    await queryRunner.query('ALTER TABLE `migrations` DROP COLUMN `batch`;');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
